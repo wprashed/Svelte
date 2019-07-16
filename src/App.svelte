0eaf219da0f2af9a6ddd8aba45634b98c697b350
Md Rashed Hossain <safanahmed5@gmail.com>
2019-07-16 09:25:15 +0600

    Chnaged

--- src/App.svelte
+++ src/App.svelte
@@ -1,5 +1,24 @@
 <script>
+	import Data from "./Data.svelte"
 	let name = 'world';
 </script>
 
-<h1>Hello {name}!</h1>
+<style>
+	.block{
+		background-color: aqua;
+		width: 900px;
+		margin: 0px auto;
+		padding: 20px 40px;
+	}
+</style>
+
+<div class="block">
+	<h1>Hello {name}</h1>
+	<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+	Minus ex, quam officia repudiandae inventore eveniet facilis quisquam quod similique libero, 
+	praesentium voluptatum dicta perferendis totam, maiores ipsam quae nisi vel.</p>
+
+	<Data name='Rashed' />
+
+	<h1>Name : {name}</h1>
+</div>
---
+++ src/Data.svelte
@@ -0,0 +1,7 @@
+<script>
+    export let name = 'Bappy';
+</script>
+
+<h1>{name}</h1>
+
+<input type="text" bind:value={name}>
